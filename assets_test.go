557a2bf4e43b62fb7a086317a85bb27f83d53c93
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-12-07 17:29:43 +0800

    :bulb: 新增注释

--- assets_test.go
+++ assets_test.go
@@ -14,6 +14,12 @@ func TestListAssets(t *testing.T) {
 	var password = os.Getenv("GO_NEXUS_PASSWORD")
 	var repository = os.Getenv("GO_NEXUS_ASSETS_REPOSITORY")
 
+	// 默认 Nexus 仓库，访问下列地址，即可产生新文件
+	// http://127.0.0.1:8081/repository/maven-central/org/springframework/boot/spring-boot/maven-metadata.xml
+	// http://127.0.0.1:8081/repository/maven-central/org/springframework/boot/spring-boot/2.7.18/spring-boot-2.7.18.pom
+	// http://127.0.0.1:8081/repository/maven-public/org/springframework/boot/spring-boot/maven-metadata.xml
+	// http://127.0.0.1:8081/repository/maven-public/org/springframework/boot/spring-boot/2.7.18/spring-boot-2.7.18.pom
+
 	client, err := NewClient(baseURL, username, password)
 	assert.NoError(t, err)
 
