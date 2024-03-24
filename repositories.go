7bda6cc54c2b091f1e3760f4ead3f12d316a5d6a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-03-24 21:42:21 +0800

    :sparkles: UploadFolder

--- repositories.go
+++ repositories.go
@@ -1,7 +1,11 @@
 package nexus
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type RepositoryService struct {
@@ -25,4 +29,84 @@ func (s *RepositoryService) ListRepository(options ...RequestOptionFunc) ([]*Rep
 	}
 
 	return repositoryXO, resp, nil
+}
+
+func (s *RepositoryService) UploadFolder(folder string, repositoryName string, options ...RequestOptionFunc) error {
+
+	mavenHostedApiRepository, _, err := s.client.Maven.GetMavenHostedRepository(repositoryName)
+	if err != nil {
+		return err
+	}
+
+	var versionPolicy = mavenHostedApiRepository.Maven.VersionPolicy
+
+	u := "repository/" + repositoryName
+
+	err = filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			// 跳过文件夹
+			return nil
+		}
+
+		fileName := filepath.Base(path)
+
+		if fileName == "desktop.ini" || fileName == "_remote.repositories" ||
+			strings.HasSuffix(fileName, ".lastUpdated") || strings.HasSuffix(fileName, ".sha1") {
+			return nil
+		}
+
+		if versionPolicy != "SNAPSHOT" && strings.Contains(fileName, "snapshots") || strings.Contains(fileName, "SNAPSHOTS") {
+			return nil
+		}
+
+		filePath := path[len(folder)+1:]
+		//tmp := filePath[:len(filePath)-len(fileName)-1]
+
+		log.Printf("上传 文件 %s 开始", path)
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		url := u + "/" + strings.ReplaceAll(filePath, "\\", "/")
+
+		req, err := s.client.UploadRequest(http.MethodPut, url, file, fileName, options)
+		if err != nil {
+			return err
+		}
+
+		resp, err := s.client.Do(req, nil)
+		log.Println(resp.Status)
+		if err != nil {
+
+			req, err = s.client.NewRequest(http.MethodGet, url, nil, file, options)
+			if err != nil {
+				return err
+			}
+
+			resp, err = s.client.Do(req, nil)
+			if err != nil {
+				return err
+			}
+
+			if resp.StatusCode == 200 {
+				log.Println("文件已存在")
+				return nil
+			}
+
+			return err
+		}
+
+		log.Printf("上传 文件 %s 结束", path)
+
+		return nil
+	})
+
+	return err
 }
--- repositories_test.go
+++ repositories_test.go
@@ -22,3 +22,19 @@ func TestListRepository(t *testing.T) {
 	assert.NotEqual(t, 0, len(repositories))
 	t.Log("Repositories Len:", len(repositories))
 }
+
+func TestUploadRepository(t *testing.T) {
+
+	var baseURL = Getenv("GO_NEXUS_BASE_URL", "http://127.0.0.1:8081/")
+	var username = Getenv("GO_NEXUS_USERNAME", "admin")
+	var password = Getenv("GO_NEXUS_PASSWORD", "password")
+
+	client, err := NewClient(baseURL, username, password)
+	assert.NoError(t, err)
+
+	folder := "D:\\Apache\\apache-maven-3.6.3-repo"
+	repositoryName := "maven-hosted"
+
+	err = client.Repository.UploadFolder(folder, repositoryName)
+	assert.NoError(t, err)
+}
